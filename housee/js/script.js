9f9389c93850770b810b558eabbf06871bb308cc
BelyakovaAsya <94910539+BelyakovaAsya@users.noreply.github.com>
2022-01-28 18:04:32 +0400

    Add files via upload

--- housee/js/script.js
+++ housee/js/script.js
@@ -1,6 +1,25 @@
 jQuery(document);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    });
+});
+
 $(function($) {
-    $('html, body').scrollTop($(document).height());
+    // $('html, body').scrollTop($(document).height());
 
     $("#scroll-top > button").on("click", function(e) {
         var body = $("html, body");
