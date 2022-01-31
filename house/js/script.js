6f2a394b3a29815497b8d1aaacfca4cdb49a7482
BelyakovaAsya <94910539+BelyakovaAsya@users.noreply.github.com>
2022-01-31 11:14:25 +0400

    Add files via upload

---
+++ house/js/script.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active', );
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active', 'body');
+            menu.classList.toggle('menu_active');
+        });
+    });
+});
+
+$(function() {
+    $('html, body').scrollTop($(document).height());
+
+    // $(window).scroll(function() {
+    //     if ($(this).scrollTop() > 1600) {
+    //         $('#scroll-top ').fadeIn();
+    //     } else {
+    //         $('#scroll-top ').fadeOut();
+    //     }
+    // });
+
+    $("#scroll-top > button").on("click", function(e) {
+        var body = $("html, body");
+        body.stop().animate({ scrollTop: 0 }, 600, "swing");
+    });
+
+
+
+    $('input[name=phone]').mask("+7(999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+});
