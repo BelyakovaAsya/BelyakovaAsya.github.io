fca63e53024547ed1157bc97afc6649e05c920fa
BelyakovaAsya <94910539+BelyakovaAsya@users.noreply.github.com>
2022-05-20 13:55:42 +0400

    Add files via upload

--- brend/js/main.js
+++ brend/js/main.js
@@ -29,42 +29,42 @@ $(function() {
                 breakpoint: 1700,
                 settings: {
                     slidesToShow: 8,
-                    slidesToScroll: 8,
+                    slidesToScroll: 8
                 }
             },
             {
                 breakpoint: 1511,
                 settings: {
                     slidesToShow: 6,
-                    slidesToScroll: 6,
+                    slidesToScroll: 6
                 }
             },
             {
                 breakpoint: 1201,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 4,
+                    slidesToScroll: 4
                 }
             },
             {
                 breakpoint: 841,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToScroll: 3
                 }
             },
             {
                 breakpoint: 551,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToScroll: 2
                 }
             },
             {
                 breakpoint: 376,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1,
+                    slidesToScroll: 1
                 }
             }
         ]
@@ -73,6 +73,7 @@ $(function() {
         prevArrow: '<button type"button" class="slick-prev article-slider__arrowleft"><img src="img/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button type"button" class="slick-prev article-slider__arrowright"><img src="img/arrow-right.svg" alt="arrow-right"></button>'
     });
+
 
 
     var mixer = mixitup('.gallery__inner', {
