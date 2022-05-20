a4a276c41f42fe7cf3407c205be8d59eaf3ca210
BelyakovaAsya <94910539+BelyakovaAsya@users.noreply.github.com>
2022-05-20 10:14:02 +0400

    Add files via upload

--- brend/js/main.js
+++ brend/js/main.js
@@ -6,6 +6,14 @@ $(function() {
     $('.rightside-menu__close').on('click', function() {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
+
+    $('.header__btn-menu').on('click', function() {
+        $('.menu').toggleClass('menu--open');
+    });
+    $('.rightside-menu__close').on('click', function() {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -17,6 +25,49 @@ $(function() {
         slidesToScroll: 10,
         dots: true,
         arrows: false,
+        responsive: [{
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 551,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 376,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
     $('.article-slider__box').slick({
         prevArrow: '<button type"button" class="slick-prev article-slider__arrowleft"><img src="img/arrow-left.svg" alt="arrow-left"></button>',
