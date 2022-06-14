27a19c988f8d5384dca519c8d828907e4acc125b
BelyakovaAsya <94910539+BelyakovaAsya@users.noreply.github.com>
2022-06-14 15:47:22 +0400

    Add files via upload

---
+++ portfolio/house vop/dist/js/script.js
@@ -0,0 +1,78 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active', );
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active', 'body');
+            menu.classList.toggle('menu_active');
+        });
+    });
+});
+
+$(function() {
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('#scroll_top').fadeIn();
+        } else {
+            $('#scroll_top').fadeOut();
+        }
+    });
+    $('#scroll_top').click(function() {
+        $('body,html').animate({ scrollTop: 0 }, 400);
+        return false;
+    });
+
+    $('input[name=phone]').mask("+7(999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
