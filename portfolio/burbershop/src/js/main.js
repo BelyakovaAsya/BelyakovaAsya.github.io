b0e141f7a423344e58960ecf7cb71b62464dcf19
BelyakovaAsya <94910539+BelyakovaAsya@users.noreply.github.com>
2022-07-08 15:57:06 +0400

    Add files via upload

---
+++ portfolio/burbershop/src/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        hamburger = document.querySelector('.hamburger');
+
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active', );
+        menu.classList.toggle('menu_active');
+    });
+
+});
